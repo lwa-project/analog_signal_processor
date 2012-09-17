db8e06a49c4417a722042282e1b80f5accafff6d
Jayce Dowell <jayce.dowell@gmail.com>
2012-09-17 19:18:05 +0000

    Added the script to push the ASP temperature readings to lwalab.

---
+++ scripts/postASPTemps.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import urllib
+import subprocess
+
+
+URL = "http://lwalab.phys.unm.edu/OpScreen/test.php"
+KEY = "c0843461abe746a4608dd9c897f9b261"
+SUBSYSTEM = "ASP"
+
+# Get the last line of the log file
+t = subprocess.Popen(["tail", "-n1", '/data/temp.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+test, junk = t.communicate()
+test = test.replace('\n', '')
+
+# Check to see if the log is actually getting updated.  If not, send NaNs
+try:
+	lastUpdated, junk = test.split(',', 1)
+except ValueError:
+	lastUpdated = 0
+	junk = []
+lastUpdated = float(lastUpdated)
+if time.time() > lastUpdated + 300:
+	test = "%.2f,%s" % (time.time(), ','.join(["NaN" for i in xrange(28*5)]))
+
+# Send the update to lwalab
+p = urllib.urlencode({'key': KEY, 'subsystem': SUBSYSTEM, 'data': test})
+f = urllib.urlopen(URL, p)
+
