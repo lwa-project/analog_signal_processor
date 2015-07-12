3f4be0edfa8e7df48e374c88fe2936ba86f8af63
Jayce Dowell <jayce.dowell@gmail.com>
2015-07-12 02:00:13 +0000

    Updated the postASPTemps.py for the new OpScreen structure.

--- scripts/postASPTemps.py
+++ scripts/postASPTemps.py
@@ -4,10 +4,12 @@
 import time
 import urllib
 import subprocess
+from socket import gethostname
 
 
 URL = "http://lwalab.phys.unm.edu/OpScreen/update.php"
 KEY = "c0843461abe746a4608dd9c897f9b261"
+SITE = gethostname().split("-",1)[0]
 SUBSYSTEM = "ASP"
 
 # Get the last line of the log file
@@ -26,6 +28,6 @@ if time.time() > lastUpdated + 300:
 	test = "%.2f,%s" % (time.time(), ','.join(["NaN" for i in xrange(4)]))
 
 # Send the update to lwalab
-p = urllib.urlencode({'key': KEY, 'subsystem': SUBSYSTEM, 'data': test})
+p = urllib.urlencode({'key': KEY, 'site': SITE, 'subsystem': SUBSYSTEM, 'data': test})
 f = urllib.urlopen(URL, p)
 
