bb304566a6836f0f1640d1cf844bef3e475da313
Seth Bruzewski <bruzewskis@gmail.com>
2022-08-01 12:43:03 -0600

    Tracing with dummies

--- aspRS485.py
+++ aspRS485.py
@@ -91,8 +91,11 @@ def rs485Get(stand, maxRetry=MAX_RS485_RETRY, waitRetry=WAIT_RS485_RETRY):
             for board in RS485_ANTENNA_MAPPING.keys():
                 for attempt in range(maxRetry+1):
                     try:
+                        aspRS485Logger.debug('Dummy1')
                         board_config = _ARX.get_all_chan_cfg(board & 0xFF)
+                        aspRS485Logger.debug('Dummy2')
                         config.extend(board_config)
+                        aspRS485Logger.debug('Dummy3')
                         break
                     except Exception as e:
                         aspRS485Logger.warning("Could not get channel info. for board %s: %s", board, str(e))
