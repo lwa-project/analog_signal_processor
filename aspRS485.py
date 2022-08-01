d57e95c8e01fe58274b01edae37c4498643bac3c
Seth Bruzewski <bruzewskis@gmail.com>
2022-08-01 12:57:26 -0600

    Longer timeout for testing again

--- aspRS485.py
+++ aspRS485.py
@@ -92,7 +92,7 @@ def rs485Get(stand, maxRetry=MAX_RS485_RETRY, waitRetry=WAIT_RS485_RETRY):
                 for attempt in range(maxRetry+1):
                     try:
                         aspRS485Logger.debug('Dummy1')
-                        dummy = _ARX._send(board&0xFF, 'geta', '', 15)
+                        #dummy = _ARX._send(board&0xFF, 'geta', '', 15)
                         aspRS485Logger.debug('Dummy2')
                         board_config = _ARX.get_all_chan_cfg(board & 0xFF, 30)
                         aspRS485Logger.debug('Dummy3')
