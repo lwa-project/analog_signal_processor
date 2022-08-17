458faa88cb07a5d43c7adcaff293e95340dc764c
Seth Bruzewski <bruzewskis@gmail.com>
2022-08-17 11:11:33 -0600

    Checking Jayce theory

--- aspRS485.py
+++ aspRS485.py
@@ -134,9 +134,8 @@ def rs485Send(stand, config, maxRetry=MAX_RS485_RETRY, waitRetry=WAIT_RS485_RETR
                 board_success = False
                 for attempt in range(maxRetry+1):
                     try:
-                        aspRS485Logger.debug(f'Before call')
+                        aspRS485Logger.debug(f'Before call'+str(len(config)))
                         _ARX.set_all_different_chan_cfg(board & 0xFF, config)
-                        aspRS485Logger.debug(f'After call')
                         board_success = True
                         break
                     except Exception as e:
