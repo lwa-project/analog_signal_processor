8ad89a8fc1532063131a61105168f872c968ca43
jaycedowell <jayce.dowell@gmail.com>
2022-11-28 07:05:10 -0700

    Move away from C and turn the SUB-20 settings into a Python module.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import numpy
+import tempfile
+import subprocess
+from setuptools import setup, Extension, find_packages
+from distutils import log
+from distutils.command.install import install
+
+
+class dummy_install(install):
+    """Dummay install method that doesn't let you install."""
+    def finalize_options(self, *args, **kwargs):
+        raise RuntimeError("This is a dummy package that cannot be installed")
+
+
+ExtensionModules = [Extension('sub20Config', ['sub20Config.cpp'], include_dirs=['libsub'], libraries=['m', 'usb-1.0'], extra_compile_args=['-std=c++11'])]
+
+
+setup(
+    cmdclass = {'install': dummy_install}, 
+    name = 'dummy_package',
+    version = '0.0',
+    description = 'This is a dummy package to help build the pulsar extensions',
+    ext_modules = ExtensionModules
+)
---
+++ sub20Config.cpp
@@ -0,0 +1,63 @@
+#include <Python.h>
+
+#include <mutex>
+
+#include "aspCommon.hpp"
+
+/*
+  Python3 Compatiability
+*/
+
+#if PY_MAJOR_VERSION >= 3
+    #define PyInt_FromLong PyLong_FromLong
+    #define PyInt_AsLong PyLong_AsLong
+    #define PyString_FromString PyUnicode_FromString
+    #define PyString_AS_STRING PyBytes_AS_STRING
+    
+    #define MOD_ERROR_VAL NULL
+    #define MOD_SUCCESS_VAL(val) val
+    #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+    #define MOD_DEF(ob, name, methods, doc) \
+       static struct PyModuleDef moduledef = { \
+          PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+       ob = PyModule_Create(&moduledef);
+#else
+    #define MOD_ERROR_VAL
+    #define MOD_SUCCESS_VAL(val)
+    #define MOD_INIT(name) PyMODINIT_FUNC init##name(void)
+    #define MOD_DEF(ob, name, methods, doc) \
+       ob = Py_InitModule3(name, methods, doc);
+#endif
+
+
+static PyMethodDef Sub20Methods[] = { {NULL, NULL,  0,  NULL} };
+};
+
+
+PyDoc_STRVAR(sub20config_doc, "Compile time configuration values used by the SUB-20 interface.");
+
+
+MOD_INIT(sub20Config) {
+  PyObject *m, *all, *value0, *value1;
+  
+  // Module definitions and functions
+  MOD_DEF(m, "sub20Config", Sub20Methods, sub20config_doc);
+  if( m == NULL ) {
+      return MOD_ERROR_VAL;
+  }
+  
+  // Constants
+  value0 = PyInt_FromLong(MAX_BOARDS);
+  PyModule_AddObject(m, "max_arx_boards", value0);
+  value1 = PyInt_FromLong(STANDS_PER_BOARD);
+  PyModule_AddObject(m, "stands_per_board", value1);
+  
+  // Module listings
+  all = PyList_New(0);
+  PyList_Append(all, PyString_FromString("max_arx_boards"));
+  PyList_Append(all, PyString_FromString("stands_per_board"));
+  PyModule_AddObject(m, "__all__", all);
+  
+  return MOD_SUCCESS_VAL(m);
+}
+  
