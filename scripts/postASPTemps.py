6340f087e06a894507a962af982b92d2d2b55bd6
jaycedowell <jayce.dowell@gmail.com>
2023-01-06 20:05:47 -0700

    Roll in recent changes to how we deal with POST data.

--- scripts/postASPTemps.py
+++ scripts/postASPTemps.py
@@ -1,24 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 import time
-import requests
 import subprocess
 from socket import gethostname
 
+from lwa_auth import KEYS as LWA_AUTH_KEYS
+from lwa_auth.signed_requests import post as signed_post
 
-URL = "https://lwalab.phys.unm.edu/OpScreen/update.php"
-KEY = "c0843461abe746a4608dd9c897f9b261"
+
+URL = "https://lwalab.phys.unm.edu/OpScreen/update"
 SITE = gethostname().split("-",1)[0]
 SUBSYSTEM = "ASP"
 
 # Get the last line of the log file
-t = subprocess.Popen(["tail", "-n1", '/data/temp.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-test, junk = t.communicate()
-try:
-    test = test.decode('ascii')
-except AttributeError:
-    pass
+test = subprocess.check_output(["tail", "-n1", '/data/temp.txt'], stderr=subprocess.DEVNULL)
+test = test.decode('ascii')
 test = test.replace('\n', '')
 
 # Check to see if the log is actually getting updated.  If not, send NaNs
@@ -32,7 +28,6 @@ if time.time() > lastUpdated + 300:
     test = "%.2f,%s" % (time.time(), ','.join(["NaN" for i in range(4)]))
 
 # Send the update to lwalab
-f = requests.post(URL,
-                  data={'key': KEY, 'site': SITE, 'subsystem': SUBSYSTEM, 'data': test})
+f = signed_post(LWA_AUTH_KEYS.get('asp', kind='private'), URL,
+                data={'site': SITE, 'subsystem': SUBSYSTEM, 'data': test})
 f.close()
-
