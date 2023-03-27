1bb6322809fd3a2a2ece1ddffdce37eae1772a67
jaycedowell <jayce.dowell@gmail.com>
2023-03-27 09:54:12 -0600

    Command marker cleanup.

--- arx_control/libatmega/libatmega.cpp
+++ arx_control/libatmega/libatmega.cpp
@@ -213,8 +213,8 @@ ssize_t atmega::send_command(atmega::handle fd, const atmega::buffer* command, a
   response->command = atmega::COMMAND_FAILURE;
   
   ssize_t nsend = 0, nrecv = 0;
-  const char start[3] = {'<', '<', '<'};
-  const char stop[3] = {'>', '>', '>'};
+  const char start[] = "<<<";
+  const char stop[] = ">>>";
   char *temp;
   temp = (char*) ::calloc(1, 6+sizeof(buffer));
   for(int i=0; i<max_retry+1; i++) {
@@ -223,9 +223,9 @@ ssize_t atmega::send_command(atmega::handle fd, const atmega::buffer* command, a
     }
     
     // Send the command
-    nsend = ::write(fd, &(start[0]), sizeof(start));
+    nsend = ::write(fd, start, sizeof(start));
     nsend += ::write(fd, command, 3+command->size);
-    nsend += ::write(fd, &(stop[0]), sizeof(stop));
+    nsend += ::write(fd, stop, sizeof(stop));
     
     // Wait
     int cmd_wait_ms = 5;
@@ -243,7 +243,7 @@ ssize_t atmega::send_command(atmega::handle fd, const atmega::buffer* command, a
     // Receive the reply
     nrecv = ::read(fd, (uint8_t*) temp, 6+sizeof(buffer));
     if( nsend >= 9 && nrecv >= 9 ) {
-      if( temp[0] == '<' && temp[1] == '<' && temp[2] == '<' ) {
+      if( (strncmp(temp, start, 3) == 0) && (strncmp(temp+nrecv-3, stop, 3) == 0) ) {
         ::memcpy((uint8_t*) response, temp+3, nrecv-6);
         break;
       }
