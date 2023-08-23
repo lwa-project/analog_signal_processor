4bb6a348c257b2241fdd9597a6e460f0aa9dba55
jaycedowell <jayce.dowell@gmail.com>
2023-08-23 08:53:16 -0600

    Update logrotate and the log archiving for the board and FEE current logs.

--- scripts/uploadLogfile.sh
+++ scripts/uploadLogfile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 ls /lwa/runtime/runtime*.gz | xargs -n1 ~ops/uploadLogfile.py
-ls /data/psu*.gz | xargs -n1 ~ops/uploadLogfile.py
+ls /data/*-power*gz | xargs -n1 ~ops/uploadLogfile.py
 ls /data/temp*gz | xargs -n1 ~ops/uploadLogfile.py
-ls /home/ops/board*.gz | xargs -n1 ~ops/uploadLogfile.py
-
