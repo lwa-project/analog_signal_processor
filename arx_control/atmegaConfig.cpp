98a9a3b53fb715efa037e9954f2e6a57469f5efd
jaycedowell <jayce.dowell@gmail.com>
2023-11-08 10:34:03 -0500

    Switch to multi-phase.

--- arx_control/atmegaConfig.cpp
+++ arx_control/atmegaConfig.cpp
@@ -3,50 +3,57 @@
 #include "aspCommon.hpp"
 
 
-static PyMethodDef ATmegaMethods[] = { {NULL, NULL, 0, NULL} };
+static PyMethodDef atmegaconfig_methods[] = { {NULL, NULL, 0, NULL} };
 
 
 PyDoc_STRVAR(atmegaconfig_doc, "Compile time configuration values used by the ATmega interface.");
 
 
-PyMODINIT_FUNC PyInit_atmegaConfig(void) {
-  PyObject *m, *all, *value0, *value1, *value2, *value3;
-  
-  // Module definitions and functions
-  static struct PyModuleDef moduledef = {
-     PyModuleDef_HEAD_INIT, "atmegaConfig", atmegaconfig_doc, -1, ATmegaMethods
-  };
-  m = PyModule_Create(&moduledef);
-  if( m == NULL ) {
-      return NULL;
-  }
-  
-  // Constants
-  value0 = PyLong_FromLong(MAX_BOARDS);
-  PyModule_AddObject(m, "MAX_BOARDS", value0);
-  value1 = PyLong_FromLong(STANDS_PER_BOARD);
-  PyModule_AddObject(m, "STANDS_PER_BOARD", value1);
-  #ifdef __USE_INPUT_CURRENT__
-    value2 = Py_True;
-  #else
-    value2 = Py_False;
-  #endif
-  PyModule_AddObject(m, "USE_INPUT_CURRENT", value2);
-  #ifdef __INCLUDE_MODULE_TEMPS__
-    value3 = Py_True;
-  #else
-    value3 = Py_False;
-  #endif
-  PyModule_AddObject(m, "INCLUDE_MODULE_TEMPS", value3);
-  
-  // Module listings
-  all = PyList_New(0);
-  PyList_Append(all, PyUnicode_FromString("MAX_BOARDS"));
-  PyList_Append(all, PyUnicode_FromString("STANDS_PER_BOARD"));
-  PyList_Append(all, PyUnicode_FromString("USE_INPUT_CURRENT"));
-  PyList_Append(all, PyUnicode_FromString("INCLUDE_MODULE_TEMPS"));
-  PyModule_AddObject(m, "__all__", all);
-  
-  return m;
+static int atmegaconfig_exec(PyObject *module) {
+    PyObject* value0 = PyLong_FromLong(MAX_BOARDS);
+    PyModule_AddObject(module, "MAX_BOARDS", value0);
+    PyObject* value1 = PyLong_FromLong(STANDS_PER_BOARD);
+    PyModule_AddObject(module, "STANDS_PER_BOARD", value1);
+    #ifdef __USE_INPUT_CURRENT__
+        PyObject* value2 = Py_True;
+    #else
+        PyObject* value2 = Py_False;
+    #endif
+    PyModule_AddObject(module, "USE_INPUT_CURRENT", value2);
+    #ifdef __INCLUDE_MODULE_TEMPS__
+        PyObject* value3 = Py_True;
+    #else
+        PyObject* value3 = Py_False;
+    #endif
+    PyModule_AddObject(module, "INCLUDE_MODULE_TEMPS", value3);
+    
+    // Module listings
+    PyObject* all = PyList_New(0);
+    PyList_Append(all, PyUnicode_FromString("MAX_BOARDS"));
+    PyList_Append(all, PyUnicode_FromString("STANDS_PER_BOARD"));
+    PyList_Append(all, PyUnicode_FromString("USE_INPUT_CURRENT"));
+    PyList_Append(all, PyUnicode_FromString("INCLUDE_MODULE_TEMPS"));
+    PyModule_AddObject(module, "__all__", all);
+    return 0;
 }
-  
+
+static PyModuleDef_Slot atmegaconfig_slots[] = {
+    {Py_mod_exec, (void *)&atmegaconfig_exec},
+    {0,           NULL}
+};
+
+static PyModuleDef atmegaconfig_def = {
+    PyModuleDef_HEAD_INIT,    /* m_base */
+    "atmegaConfig",           /* m_name */
+    atmegaconfig_doc,         /* m_doc */
+    0,                        /* m_size */
+    atmegaconfig_methods,     /* m_methods */
+    atmegaconfig_slots,       /* m_slots */
+    NULL,                     /* m_traverse */
+    NULL,                     /* m_clear */
+    NULL,                     /* m_free */
+};
+
+PyMODINIT_FUNC PyInit_atmegaConfig(void) {
+  return PyModuleDef_Init(&atmegaconfig_def);
+}
