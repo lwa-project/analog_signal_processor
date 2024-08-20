2de3a31d3acb6a59cf0295249224c8d146614234
jaycedowell <jayce.dowell@gmail.com>
2024-08-20 16:47:41 -0600

    Rein the retries to better deal with the spectrum analyzer.

--- arx_control/listATmegaSN.cpp
+++ arx_control/listATmegaSN.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
       cmd.command = atmega::COMMAND_READ_SN;
       cmd.size = 0;
       
-      int n = atmega::send_command(fd, &cmd, &resp, ATMEGA_OPEN_MAX_ATTEMPTS, ATMEGA_OPEN_WAIT_MS);
+      int n = atmega::send_command(fd, &cmd, &resp, std::min(3, ATMEGA_OPEN_MAX_ATTEMPTS), ATMEGA_OPEN_WAIT_MS);
       if( (n > 0) && (resp.command & atmega::COMMAND_FAILURE) == 0 ) {
         std::string sn;
         for(int i=0; i<std::min((uint16_t) 8, (uint16_t) resp.size); i++) {
