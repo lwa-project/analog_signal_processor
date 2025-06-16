3c5e93f6f3c52f6e8922c203b2edacf598f6725d
jaycedowell <jayce.dowell@gmail.com>
2025-06-16 16:09:41 -0600

    Try PyBind11 to expose the ATmega class for troubleshooting the power supplies.

--- arx_control/setup.py
+++ arx_control/setup.py
@@ -4,6 +4,7 @@ import subprocess
 from setuptools import setup, Extension, find_packages
 from distutils import log
 from distutils.command.install import install
+from pybind11.setup_helpers import Pybind11Extension as PBE, build_ext
 
 
 class dummy_install(install):
@@ -12,13 +13,14 @@ class dummy_install(install):
         raise RuntimeError("This is a dummy package that cannot be installed")
 
 
-ExtensionModules = [Extension('atmegaConfig', ['atmegaConfig.cpp'], include_dirs=['libatmega'], libraries=['m', 'atmega'], extra_compile_args=['-std=c++17'], extra_link_args=['-Llibatmega'])]
+ExtensionModules = [Extension('atmegaConfig', ['atmegaConfig.cpp'], include_dirs=['libatmega'], libraries=['m', 'atmega'], extra_compile_args=['-std=c++17'], extra_link_args=['-Llibatmega']),
+                    PBE('atmegaWrap', ['atmegaWrap.cpp'], include_dirs=['libatmega'], libraries=["atmega"],  extra_compile_args=['-std=c++17'], extra_link_args=['-Llibatmega'])]
 
 
 setup(
-    cmdclass = {'install': dummy_install}, 
+    cmdclass = {'build_ext': build_ext, 'install': dummy_install}, 
     name = 'dummy_package',
     version = '0.0',
-    description = 'This is a dummy package to help build the pulsar extensions',
+    description = 'This is a dummy package to help build the ATmega Python tools',
     ext_modules = ExtensionModules
 )
