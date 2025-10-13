a0daccbf05f08b56a48c3d2270b13278bb0ce114
jaycedowell <jaycedowell@gmail.com>
2025-10-13 12:57:29 -0600

    Try a little bit harder.

--- aspSUB20.py
+++ aspSUB20.py
@@ -24,13 +24,13 @@ __all__ = ['spiCountBoards', 'SPICommandCallback', 'SPIProcessingThread', 'psuSe
            'SPI_P20_on', 'SPI_P20_off', 'SPI_P21_on', 'SPI_P21_off', 'SPI_P22_on', 'SPI_P22_off', 'SPI_P23_on', 'SPI_P23_off',
            'SPI_P24_on', 'SPI_P24_off', 'SPI_P25_on', 'SPI_P25_off', 'SPI_P26_on', 'SPI_P26_off', 'SPI_P27_on', 'SPI_P27_off',
            'SPI_P28_on', 'SPI_P28_off', 'SPI_P29_on', 'SPI_P29_off', 'SPI_P30_on', 'SPI_P30_off', 'SPI_P31_on', 'SPI_P31_off',
-           'SPI_NoOp']
+           'SPI_NoOp', 'MAX_SPI_RETRY']
 
 
 aspSUB20Logger = logging.getLogger('__main__')
 
 # SPI control
-MAX_SPI_RETRY = 0
+MAX_SPI_RETRY = 2
 WAIT_SPI_RETRY = 0.2
 
 
